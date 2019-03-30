42753d1b89c6520cdb888e2249b9bf4ea584a2b7
brutto <hongtaoz@bu.edu>
2019-03-29 23:53:59 -0400

    HW3 update

--- HW3/routes/hw3.js
+++ HW3/routes/hw3.js
@@ -1,9 +1,28 @@
+"use strict";
+
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+var ForecastIO = require('forecast-io');
+var forecast = new ForecastIO('API-KEY');
+
+/**
+ * GET /forecast
+ */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+   forecast
+    .latitude('42.3601')
+    .longitude('71.0589') //for Boston
+    .get()
+    .then(function(forecast) {
+        console.log(forecast);
+        res.send(forecast);
+    })
+    .catch(function(err)  {
+        console.log(err);
+        res.send(err);
+    });
 });
 
 module.exports = router;
